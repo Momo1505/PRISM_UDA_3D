8054588258c30757a233768f913ab3fecca7aa69
Mouhamed Sow <mouha052002@gmail.com>
2025-09-09 09:32:53 +0200

    noise modification

--- launch_train.sh
+++ launch_train.sh
@@ -5,7 +5,7 @@ sbatch slurm_scripts/I3toLW4_mic_hrda_noise_3.slurm
 
 sbatch slurm_scripts/I3toLW4_mic_hrda_noise_5.slurm
 
-sbatch slurm_scripts/I3toLW4_mic_hrda_noise_20.slurm
+sbatch slurm_scripts/I3toLW4_mic_hrda_noise_2.slurm
 
 sbatch slurm_scripts/I3toLW4_mic_hrda_noise_4.slurm
 
